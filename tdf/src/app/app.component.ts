21ab03d969956f83574856d15fef360fd7d89818
Joseph Escue <joseph.escue@comcast.net>
2019-06-18 18:18:53 -0500

    Lessons 8 - 10 Form Validation

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ import { User } from './user';
 export class AppComponent {
   title = 'tdf';
   topics = [ 'Angular', 'React',  'Vue'];
+  topicHasError = true;
 
-  userModel = new User('Rob', 'rob@test.com', 1112223333, '', 'morning', true );
+  userModel = new User('Rob', 'rob@test.com', 1112223333, 'default', 'morning', true );
+
+  validateTopic(value){
+    if (value === 'default'){
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
 }
