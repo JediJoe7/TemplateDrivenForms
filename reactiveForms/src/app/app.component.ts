dc7ec50dfac839476545f8923c46ba6b3767e4fa
Joseph Escue <joseph.escue@comcast.net>
2019-06-29 15:47:06 -0500

    Lesson 27 -Submitting Reactive Forms

--- reactiveForms/src/app/app.component.ts
+++ reactiveForms/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/user-name,validator';
 import { PasswordValidator } from './shared/password.validator';
+import { RegistrationService } from './registration.service';
 
 @Component({
   selector: 'app-root',
@@ -28,7 +29,8 @@ export class AppComponent implements OnInit {
     this.alternateEmails.push(this.fb.control(''));
   }
 
-  constructor(private fb: FormBuilder ) {}
+// tslint:disable-next-line: variable-name
+  constructor(private fb: FormBuilder, private _registrationService: RegistrationService ) {}
 
   ngOnInit() {
 
@@ -76,5 +78,14 @@ export class AppComponent implements OnInit {
       password: 'test',
       confirmPassword: 'test'
     });
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+    this._registrationService.register(this.registrationForm.value)
+      .subscribe(
+        response => console.log('Success', response),
+        error => console.error('Error!', error)
+      );
   }
 }
